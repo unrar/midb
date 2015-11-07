0b95b572ca4d574c1cbeae84c3e4291c4301b5e6
unrar <catbuntu@catbuntu.me>
2015-11-07 13:12:18 +0100

    :smirk: Trying to reduce complexity

--- lib/midb/server_controller.rb
+++ lib/midb/server_controller.rb
@@ -2,6 +2,7 @@ require 'midb/server_model'
 require 'midb/server_view'
 require 'midb/errors_view'
 require 'midb/security_controller'
+require 'midb/serverengine_controller'
 
 require 'yaml'
 require 'socket'
@@ -38,7 +39,195 @@ module MIDB
     @config = Hash.new()
     @port = 8081
 
-    # Decide the server's behavior depending on the arguments. 
+    #####################
+    #  Server commands  #
+    #####################
+
+    # $ midb help
+    #
+    # Show some help for either midb or a command.
+    def self.do_help()
+      if @args.length > 1
+        case @args[1]
+        when "bootstrap"
+          MIDB::ServerView.help(:bootstrap)
+        when "set"
+          MIDB::ServerView.help(:set)
+        when "start"
+          MIDB::ServerView.help(:start)
+        when "serve"
+          MIDB::ServerView.help(:serve)
+        when "unserve"
+          MIDB::ServerView.help(:unserve)
+        else
+          MIDB::ErrorsView.die(:no_help)
+        end
+      else
+        MIDB::ServerView.help(:list)
+      end
+    end
+
+    # $ midb bootstrap
+    #
+    # Bootstrap a new midb project in the active directory.
+    def self.do_bootstrap()
+      if File.file?(".midb.yaml")
+        MIDB::ErrorsView.die(:already_project)
+      else
+        # If the file doesn't exist it, create it with the default stuff
+        @config["serves"] = []
+        @config["status"] = :asleep    # The server is initially asleep
+        @config["apikey"] = "midb-api" # This should be changed, it's the private API key
+        @config["dbengine"] = :sqlite3  # SQLite is the default engine
+        # Default DB configuration for MySQL and other engines
+        @config["dbhost"] = "localhost"
+        @config["dbport"] = 3306
+        @config["dbuser"] = "nobody"
+        @config["dbpassword"] = "openaccess" 
+        File.open(".midb.yaml", 'w') do |l|
+          l.write @config.to_yaml
+        end
+        # Create json/ and db/ directory if it doesn't exist 
+        Dir.mkdir("json") unless File.exists?("json")
+        Dir.mkdir("db") unless File.exists?("db")
+        MIDB::ServerView.info(:bootstrap)
+      end
+    end
+
+    # $ midb set
+    #
+    # Set the config for the project.
+    # Check syntax
+    def self.do_set()
+      MIDB::ErrorsView.die(:syntax) if @args.length < 2
+      subset = @args[1].split(":")[0]
+      subcmd = @args[1].split(":")[1]
+      set = @args.length < 3 ? false : true
+      setter = @args[2] if set
+      case subset
+      when "db"
+        # DB Config
+        case subcmd
+        when "engine"
+          if set
+            @config["dbengine"] = case setter.downcase
+                                  when "sqlite3" then :sqlite3
+                                  when "mysql" then :mysql
+                                  else :undef
+                                  end
+            if @config["dbengine"] == :undef
+              MIDB::ErrorsView.die(:unsupported_engine)
+              @config["dbengine"] = :sqlite3
+            end
+          end
+          MIDB::ServerView.out_config(:dbengine)
+        when "host"
+          @config["dbhost"] = setter if set
+          MIDB::ServerView.out_config(:dbhost)
+        when "port"
+          @config["dbport"] = setter if set
+          MIDB::ServerView.out_config(:dbport)
+        when "user"
+          @config["dbuser"] = setter if set
+          MIDB::ServerView.out_config(:dbuser)
+        when "password"
+          @config["dbpassword"] = setter if set
+          MIDB::ServerView.out_config(:dbpassword)
+        else
+          MIDB::ErrorsView.die(:synax)
+        end
+      when "api"
+        case subcmd
+        when "key"
+          @config["apikey"] = setter if set
+          MIDB::ServerView.out_config(:apikey)
+        end
+      else
+        MIDB::ErrorsView.die(:syntax)
+      end
+    end
+
+    # $ midb start
+    #
+    # Start the server (call the loop)
+    def self.do_start()
+      # Check syntax
+      MIDB::ErrorsView.die(:syntax) if @args.length < 2
+      MIDB::ErrorsView.die(:syntax) if @args[1].split(":")[0] != "db"
+      # Is the server already started?
+      MIDB::ErrorsView.die(:server_already_started) if @config["status"] == :running
+      # Are any files being served?
+      MIDB::ErrorsView.die(:no_serves) if @config["serves"].length == 0
+      # If it successfully starts, change our status and notify thru view
+      @args.each do |arg|
+        if arg.split(":")[0] == "db"
+          @db = arg.split(":")[1]
+        elsif arg.split(":")[0] == "port"
+          @port = arg.split(":")[1]
+        end
+      end
+
+      # Call the server engine
+      if MIDB::ServerEngineController.start(@port)
+        @config["status"] = :running
+        MIDB::ServerView.success()
+      else
+        MIDB::ErrorsView.die(:server_error)
+      end
+    end
+
+    # $ midb serve
+    #
+    # Serve a JSON file
+    def self.do_serve()
+      # Check if there's a second argument
+      MIDB::ErrorsView.die(:syntax) if @args.length < 2
+      # Is the server running? It shouldn't
+      MIDB::ErrorsView.die(:server_already_started) if @config["status"] == :running
+      # Is there such file as @args[1]?
+      MIDB::ErrorsView.die(:file_404) unless File.file?("./json/" + @args[1])
+      # Is the file a JSON file?
+      MIDB::ErrorsView.die(:not_json) unless File.extname(@args[1]) == ".json"
+      # Is the file already loaded?
+      MIDB::ErrorsView.die(:json_exists) if @config["serves"].include? @args[1]
+
+      # Tests passed, so let's add the file to the served list!
+      @config["serves"].push @args[1]
+      MIDB::ServerView.show_serving()
+    end
+
+    # $ midb unserve
+    #
+    # Stop serving a JSON file
+    def self.do_unserve()
+      # Check if there's a second argument
+      MIDB::ErrorsView.die(:syntax) if @args.length < 2
+      # Is the server running? It shouldn't
+      MIDB::ErrorsView.die(:server_already_started) if @config["status"] == :running
+      # Is the file already loaded?
+      MIDB::ErrorsView.die(:json_not_exists) unless @config["serves"].include? @args[1]
+
+      # Delete it!
+      @config["serves"].delete @args[1]
+      MIDB::ServerView.show_serving()
+    end
+
+    # $ midb stop
+    #
+    # Stop the server in case it's running in the background.
+    def self.do_stop()
+      # Is the server running?
+      MIDB::ErrorsView.die(:server_not_running) unless @config["status"] == :running
+
+      @config["status"] = :asleep
+      MIDB::ServerView.server_stopped()
+    end
+
+
+
+    # $ midb
+    #
+    # Decide the behavior of the server in function of the arguments.
     def self.init()
       # We should have at least one argument, which can be `run` or `serve`
       MIDB::ErrorsView.die(:noargs) if @args.length < 1
@@ -54,286 +243,37 @@ module MIDB
       case @args[0]
 
       # Command: help
-      # Shows the help
       when "help"
-        if @args.length > 1
-          case @args[1]
-          when "bootstrap"
-            MIDB::ServerView.help(:bootstrap)
-          when "set"
-            MIDB::ServerView.help(:set)
-          when "start"
-            MIDB::ServerView.help(:start)
-          when "serve"
-            MIDB::ServerView.help(:serve)
-          when "unserve"
-            MIDB::ServerView.help(:unserve)
-          else
-            MIDB::ErrorsView.die(:no_help)
-          end
-        else
-          MIDB::ServerView.help(:list)
-        end
+        self.do_help()
 
       # Command: bootstrap
-      # Create config file and initial directories
       when "bootstrap"
-        if File.file?(".midb.yaml")
-          MIDB::ErrorsView.die(:already_project)
-        else
-          # If the file doesn't exist it, create it with the default stuff
-          @config["serves"] = []
-          @config["status"] = :asleep    # The server is initially asleep
-          @config["apikey"] = "midb-api" # This should be changed, it's the private API key
-          @config["dbengine"] = :sqlite3  # SQLite is the default engine
-          # Default DB configuration for MySQL and other engines
-          @config["dbhost"] = "localhost"
-          @config["dbport"] = 3306
-          @config["dbuser"] = "nobody"
-          @config["dbpassword"] = "openaccess" 
-          File.open(".midb.yaml", 'w') do |l|
-            l.write @config.to_yaml
-          end
-          # Create json/ and db/ directory if it doesn't exist 
-          Dir.mkdir("json") unless File.exists?("json")
-          Dir.mkdir("db") unless File.exists?("db")
-          MIDB::ServerView.info(:bootstrap)
-        end
+        self.do_bootstrap()
     
       # Command: set
-      # Sets configuration factors.
       when "set"
-        # Check syntax
-        MIDB::ErrorsView.die(:syntax) if @args.length < 2
-        subset = @args[1].split(":")[0]
-        subcmd = @args[1].split(":")[1]
-        set = @args.length < 3 ? false : true
-        setter = @args[2] if set
-        case subset
-        when "db"
-          # DB Config
-          case subcmd
-          when "engine"
-            if set
-              @config["dbengine"] = case setter.downcase
-                                    when "sqlite3" then :sqlite3
-                                    when "mysql" then :mysql
-                                    else :undef
-                                    end
-              if @config["dbengine"] == :undef
-                MIDB::ErrorsView.die(:unsupported_engine)
-                @config["dbengine"] = :sqlite3
-              end
-            end
-            MIDB::ServerView.out_config(:dbengine)
-          when "host"
-            @config["dbhost"] = setter if set
-            MIDB::ServerView.out_config(:dbhost)
-          when "port"
-            @config["dbport"] = setter if set
-            MIDB::ServerView.out_config(:dbport)
-          when "user"
-            @config["dbuser"] = setter if set
-            MIDB::ServerView.out_config(:dbuser)
-          when "password"
-            @config["dbpassword"] = setter if set
-            MIDB::ServerView.out_config(:dbpassword)
-          else
-            MIDB::ErrorsView.die(:synax)
-          end
-        when "api"
-          case subcmd
-          when "key"
-            @config["apikey"] = setter if set
-            MIDB::ServerView.out_config(:apikey)
-          end
-        else
-          MIDB::ErrorsView.die(:syntax)
-        end
+        self.do_set()
 
 
       # Command: start
-      # Starts the server
       when "start"
-        # Check syntax
-        MIDB::ErrorsView.die(:syntax) if @args.length < 2
-        MIDB::ErrorsView.die(:syntax) if @args[1].split(":")[0] != "db"
-        # Is the server already started?
-        MIDB::ErrorsView.die(:server_already_started) if @config["status"] == :running
-        # Are any files being served?
-        MIDB::ErrorsView.die(:no_serves) if @config["serves"].length == 0
-        # If it successfully starts, change our status and notify thru view
-        @args.each do |arg|
-          if arg.split(":")[0] == "db"
-            @db = arg.split(":")[1]
-          elsif arg.split(":")[0] == "port"
-            @port = arg.split(":")[1]
-          end
-        end
-
-        if self.start(@port)
-          @config["status"] = :running
-          MIDB::ServerView.success()
-        else
-          MIDB::ErrorsView.die(:server_error)
-        end
+        self.do_start()
 
       # Command: serve
       # Serves a JSON file
       when "serve"
-        # Check if there's a second argument
-        MIDB::ErrorsView.die(:syntax) if @args.length < 2
-        # Is the server running? It shouldn't
-        MIDB::ErrorsView.die(:server_already_started) if @config["status"] == :running
-        # Is there such file as @args[1]?
-        MIDB::ErrorsView.die(:file_404) unless File.file?("./json/" + @args[1])
-        # Is the file a JSON file?
-        MIDB::ErrorsView.die(:not_json) unless File.extname(@args[1]) == ".json"
-        # Is the file already loaded?
-        MIDB::ErrorsView.die(:json_exists) if @config["serves"].include? @args[1]
-
-        # Tests passed, so let's add the file to the served list!
-        @config["serves"].push @args[1]
-        MIDB::ServerView.show_serving()
+        self.do_serve()
 
       # Command: unserve
       # Stop serving a JSON file.
       when "unserve"
-        # Check if there's a second argument
-        MIDB::ErrorsView.die(:syntax) if @args.length < 2
-        # Is the server running? It shouldn't
-        MIDB::ErrorsView.die(:server_already_started) if @config["status"] == :running
-        # Is the file already loaded?
-        MIDB::ErrorsView.die(:json_not_exists) unless @config["serves"].include? @args[1]
-
-        # Delete it!
-        @config["serves"].delete @args[1]
-        MIDB::ServerView.show_serving()
+        self.do_unserve()
 
       # Command: stop
       # Stops the server.
       when "stop"
-        # Is the server running?
-        MIDB::ErrorsView.die(:server_not_running) unless @config["status"] == :running
-
-        @config["status"] = :asleep
-        MIDB::ServerView.server_stopped()
+        self.do_stop()
       end
-    end
-
-    # Method: start
-    # Starts the server on the given port (default: 8080)
-    def self.start(port=8081)
-      serv = TCPServer.new("localhost", port)
-      MIDB::ServerView.info(:start, port)
-
-      # Manage the requests
-      loop do
-        socket = serv.accept
-        MIDB::ServerView.info(:incoming_request, socket.addr[3])
-
-        request = self.parse_request(socket.gets)
-
-        # Get a hash with the headers
-        headers = {}
-        while line = socket.gets.split(' ', 2)
-          break if line[0] == "" 
-          headers[line[0].chop] = line[1].strip
-        end
-        data = socket.read(headers["Content-Length"].to_i)
-
-
-        MIDB::ServerView.info(:request, request)
-        response_json = Hash.new()
-
-        # Endpoint syntax: ["", FILE, ID, (ACTION)]
-        endpoint = request[1].split("/")
-        ep_file = endpoint[1]
-
-        method = request[0]
-        endpoints = [] # Valid endpoints
-
-        # Load the JSON served files
-        @config["serves"].each do |js|
-          # The filename is a valid endpoint
-          endpoints.push File.basename(js, ".*")
-        end
-
-        # Load the endpoints
-        found = false
-        endpoints.each do |ep|
-          if ep_file == ep
-            found = true
-            MIDB::ServerView.info(:match_json, ep)
-            # Analyze the request and pass it to the model
-            if method == "GET"
-              case endpoint.length
-              when 2
-                # No ID has been specified. Return all the entries
-                # Pass it to the model and get the JSON
-                response_json = MIDB::ServerModel.get_all_entries(@db, ep).to_json
-              when 3
-                # An ID has been specified. Should it exist, return all of its entries.
-                response_json = MIDB::ServerModel.get_entries(@db, ep, endpoint[2]).to_json
-              end
-            else
-              # An action has been specified. We're going to need HTTP authentification here.
-              MIDB::ServerView.info(:auth_required)
-
-              if (not headers.has_key? "Authentication") ||
-                 (not MIDB::SecurityController.check?(headers["Authentication"], data, @config["apikey"]))
-                @http_status = "401 Unauthorized"
-                response_json = MIDB::ServerView.json_error(401, "Unauthorized").to_json
-                MIDB::ServerView.info(:no_auth)
-
-              else
-                MIDB::ServerView.info(:auth_success)
-                if method == "POST"
-                  response_json = MIDB::ServerModel.post(@db, ep, data).to_json
-                else
-                  if endpoint.length >= 3
-                    if method == "DELETE"
-                      response_json = MIDB::ServerModel.delete(@db, ep, endpoint[2]).to_json 
-                    elsif method == "PUT"
-                      response_json = MIDB::ServerModel.put(@db, ep, endpoint[2], data).to_json
-                    end
-                  else
-                    @http_status = "404 Not Found"
-                    response_json = MIDB::ServerView.json_error(404, "Must specify an ID.").to_json
-                  end
-                end
-              end
-            end
-            MIDB::ServerView.info(:response, response_json)
-            # Return the results via HTTP
-            socket.print "HTTP/1.1 #{@http_status}\r\n" +
-                        "Content-Type: text/json\r\n" +
-                        "Content-Length: #{response_json.size}\r\n" +
-                        "Connection: close\r\n"
-            socket.print "\r\n"
-            socket.print response_json
-            socket.print "\r\n"
-            MIDB::ServerView.info(:success)
-          end
-        end
-        unless found
-          MIDB::ServerView.info(:not_found)
-          response = MIDB::ServerView.json_error(404, "Invalid API endpoint.").to_json
-
-          socket.print "HTTP/1.1 404 Not Found\r\n" +
-                       "Content-Type: text/json\r\n" +
-                       "Content-Length: #{response.size}\r\n" +
-                       "Connection: close\r\n"
-          socket.print "\r\n"
-          socket.print response
-        end
-      end
-    end
-
-    # Method: parse_request
-    # Parses an HTTP requests and returns an array [method, uri]
-    def self.parse_request(req)
-      [req.split(" ")[0], req.split(" ")[1]]
     end
 
     # Method: save
---
+++ lib/midb/serverengine_controller.rb
@@ -0,0 +1,146 @@
+require 'midb/server_model'
+require 'midb/server_view'
+require 'midb/errors_view'
+require 'midb/security_controller'
+require 'midb/server_controller'
+
+require 'yaml'
+require 'socket'
+require 'uri'
+require 'json'
+require 'sqlite3'
+
+module MIDB
+  # @author unrar
+  # This class handles runs the server engine using sockets and a loop.
+  class ServerEngineController
+    # Attribute declaration here
+    class << self
+      # @!attribute config
+      #   @return [Hash] Contains the project's configuration, saved in .midb.yaml
+      # @!attribute db
+      #   @return [String] Database name (if SQLite is the engine, file name without extension)
+      # @!attribute http_status
+      #   @return [String] HTTP status code and string representation for the header
+      attr_accessor :config, :db, :http_status
+    end
+    # Copy these values from the server controller
+    @http_status = MIDB::ServerController.http_status
+    @config = MIDB::ServerController.config
+    @db = MIDB::ServerController.db
+
+    # Starts the server on a given port (default: 8081)
+    #
+    # @param port [Fixnum] Port to which the server will listen.
+    def self.start(port=8081)
+      serv = TCPServer.new("localhost", port)
+      MIDB::ServerView.info(:start, port)
+
+      # Manage the requests
+      loop do
+        socket = serv.accept
+        MIDB::ServerView.info(:incoming_request, socket.addr[3])
+
+        request = self.parse_request(socket.gets)
+
+        # Get a hash with the headers
+        headers = {}
+        while line = socket.gets.split(' ', 2)
+          break if line[0] == "" 
+          headers[line[0].chop] = line[1].strip
+        end
+        data = socket.read(headers["Content-Length"].to_i)
+
+
+        MIDB::ServerView.info(:request, request)
+        response_json = Hash.new()
+
+        # Endpoint syntax: ["", FILE, ID, (ACTION)]
+        endpoint = request[1].split("/")
+        ep_file = endpoint[1]
+
+        method = request[0]
+        endpoints = [] # Valid endpoints
+
+        # Load the JSON served files
+        @config["serves"].each do |js|
+          # The filename is a valid endpoint
+          endpoints.push File.basename(js, ".*")
+        end
+
+        # Load the endpoints
+        found = false
+        endpoints.each do |ep|
+          if ep_file == ep
+            found = true
+            MIDB::ServerView.info(:match_json, ep)
+            # Analyze the request and pass it to the model
+            if method == "GET"
+              case endpoint.length
+              when 2
+                # No ID has been specified. Return all the entries
+                # Pass it to the model and get the JSON
+                response_json = MIDB::ServerModel.get_all_entries(@db, ep).to_json
+              when 3
+                # An ID has been specified. Should it exist, return all of its entries.
+                response_json = MIDB::ServerModel.get_entries(@db, ep, endpoint[2]).to_json
+              end
+            else
+              # An action has been specified. We're going to need HTTP authentification here.
+              MIDB::ServerView.info(:auth_required)
+
+              if (not headers.has_key? "Authentication") ||
+                 (not MIDB::SecurityController.check?(headers["Authentication"], data, @config["apikey"]))
+                @http_status = "401 Unauthorized"
+                response_json = MIDB::ServerView.json_error(401, "Unauthorized").to_json
+                MIDB::ServerView.info(:no_auth)
+
+              else
+                MIDB::ServerView.info(:auth_success)
+                if method == "POST"
+                  response_json = MIDB::ServerModel.post(@db, ep, data).to_json
+                else
+                  if endpoint.length >= 3
+                    if method == "DELETE"
+                      response_json = MIDB::ServerModel.delete(@db, ep, endpoint[2]).to_json 
+                    elsif method == "PUT"
+                      response_json = MIDB::ServerModel.put(@db, ep, endpoint[2], data).to_json
+                    end
+                  else
+                    @http_status = "404 Not Found"
+                    response_json = MIDB::ServerView.json_error(404, "Must specify an ID.").to_json
+                  end
+                end
+              end
+            end
+            MIDB::ServerView.info(:response, response_json)
+            # Return the results via HTTP
+            socket.print "HTTP/1.1 #{@http_status}\r\n" +
+                        "Content-Type: text/json\r\n" +
+                        "Content-Length: #{response_json.size}\r\n" +
+                        "Connection: close\r\n"
+            socket.print "\r\n"
+            socket.print response_json
+            socket.print "\r\n"
+            MIDB::ServerView.info(:success)
+          end
+        end
+        unless found
+          MIDB::ServerView.info(:not_found)
+          response = MIDB::ServerView.json_error(404, "Invalid API endpoint.").to_json
+
+          socket.print "HTTP/1.1 404 Not Found\r\n" +
+                       "Content-Type: text/json\r\n" +
+                       "Content-Length: #{response.size}\r\n" +
+                       "Connection: close\r\n"
+          socket.print "\r\n"
+          socket.print response
+        end
+      end
+    end
+
+    # Method: parse_request
+    # Parses an HTTP requests and returns an array [method, uri]
+    def self.parse_request(req)
+      [req.split(" ")[0], req.split(" ")[1]]
+    end
